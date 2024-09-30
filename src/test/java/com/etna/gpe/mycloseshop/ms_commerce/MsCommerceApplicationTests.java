02cc0debfd94eb54644ce30624d8d92faff156d9
amara_s <amara_s@etna-alternance.net>
2024-09-30 11:39:17 +0200

    feat(init): Initialisation du projet ms-commerce

---
+++ src/test/java/com/etna/gpe/mycloseshop/ms_commerce/MsCommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.etna.gpe.mycloseshop.ms_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// TODO document why this method is empty
+	}
+
+}
